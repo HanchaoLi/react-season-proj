b46a0262803372471c87cdb45c7cbe57b933d586
HanchaoLi <seraphme1234@gmail.com>
2019-04-05 17:15:51 -0400

    add event handler and pervent form action for now

--- pics/src/components/SearchBar.js
+++ pics/src/components/SearchBar.js
@@ -1,23 +1,32 @@
-import React from 'react';
+import React from "react";
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { term: "" };
+  }
 
-    onInputChange(event) {
-        console.log(event.target.value);
-    }
+  onFormSubmit = event => {
+    event.preventDefault();
+    console.log(this.state.term);
+  };
 
-    render() {
-        return (
-            <div className="ui segment">
-                <form className="ui form">
-                    <div className="field">
-                        <label>Image Search</label>
-                        <input type="text" onChange={this.onInputChange} />
-                    </div>
-                </form>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="ui segment">
+        <form onSubmit={this.onFormSubmit} className="ui form">
+          <div className="field">
+            <label>Image Search</label>
+            <input
+              type="text"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;

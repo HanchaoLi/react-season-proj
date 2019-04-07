87acef6796f358f813cd6c26095e128c0fb199d8
HanchaoLi <seraphme1234@gmail.com>
2019-04-07 15:56:42 -0400

    PRA-003

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import SearchBar from './SearchBar'
+import React from "react";
+import axios from "axios";
+import SearchBar from "./SearchBar";
 
-const App = () => {
+class App extends React.Component {
+  state = { images: [] };
+  onSearchSubmit = async term => {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID ddfd1818b73b0556ca485a65d990749d370e0f03117242275a7a852c3bf24779"
+      }
+    });
+    this.setState({ images: response.data.results });
+  };
+
+  render() {
     return (
-        <div className="ui container" style={{ marginTop: '10px' }}>
-            <SearchBar />
-        </div>
+      <div className="ui container" style={{ marginTop: "10px" }}>
+        <SearchBar onSubmit={this.onSearchSubmit} />
+        Found : {this.state.images.length} images
+      </div>
     );
+  }
 }
 
 export default App;

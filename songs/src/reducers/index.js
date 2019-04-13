909a7bc74d7b5716d6949c61b3a48b183fea3a10
HanchaoLi <seraphme1234@gmail.com>
2019-04-13 12:42:54 -0400

    PRA-006: add song list and song detail FED implemented using redux

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,22 @@
+import { combineReducers } from "redux";
+
+const songsReducer = () => {
+  return [
+    { title: "No Scrubs", duration: "4:05" },
+    { title: "macarena", duration: "2:03" },
+    { title: "all star", duration: "3:15" },
+    { title: "I dont wonna it", duration: "1:45" }
+  ];
+};
+
+const selectedSongReducer = (selectedSong = null, action) => {
+  if (action.type === "SONG_SELECTED") {
+    return action.payload;
+  }
+  return selectedSong;
+};
+
+export default combineReducers({
+  songs: songsReducer,
+  selectedSong: selectedSongReducer
+});

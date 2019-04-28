6a1c3cfb0d33c6dcb5033fe320b3925ee14ce567
HanchaoLi <seraphme1234@gmail.com>
2019-04-28 15:03:11 -0400

    fetch date from post API

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -3,7 +3,7 @@ import jsonPalceholder from '../apis/jsonPlaceholder';
 
 export const fetchPosts = () => async dispatch => {
     const response = await jsonPalceholder.get('/posts');
-    dispatch({ type: 'FETCH_POSTS', payload: response });
+    dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
 

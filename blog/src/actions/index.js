dec7e0b3931cd361c7de51ca20d870e64a79ca5e
hanchao.li <hanchao.li@borngroup.com>
2019-05-02 16:11:05 -0400

    PRA-008: refactor fetch user to memorize way by invoke lodash module

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,3 +1,5 @@
+/* global _ */
+import lodash from 'lodash';
 import jsonPalceholder from '../apis/jsonPlaceholder';
 
 
@@ -6,7 +8,16 @@ export const fetchPosts = () => async dispatch => {
     dispatch({ type: 'FETCH_POSTS', payload: response.data });
 };
 
-export const fetchUser = (id) => async dispatch => {
+// a way to memorize fetch result
+export const fetchUser = id =>  dispatch => _fetchUser(id, dispatch);
+
+const _fetchUser = _.memoize(async (id, dispatch) => {
     const response = await jsonPalceholder.get('/users/' + id);
     dispatch({ type: 'FETCH_USER', payload: response.data });
-};
+});
+
+
+// export const fetchUser = id =>  async dispatch => {
+//     const response = await jsonPalceholder.get('/users/' + id);
+//     dispatch({ type: 'FETCH_USER', payload: response.data });
+// };
